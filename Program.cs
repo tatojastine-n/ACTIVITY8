45a33f9d57e70f4c6308abdb3d069fd8c517d05b
tatojastine-n <jajanicoletato@gmail.com>
2025-08-07 13:51:22 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertMe
+{
+    internal class Program
+    {
+        private static string input;
+        static void Main(string[] args)
+        {
+            int number;
+            bool isValid;
+
+            do
+            {
+                Console.Write("Enter numeric input (digits only): ");
+                input = Console.ReadLine();
+
+                // Validate input contains only digits using regex
+                isValid = !string.IsNullOrWhiteSpace(input) && System.Text.RegularExpressions.Regex.IsMatch( input, @"^\d+$");
+
+                if (!isValid)
+                {
+                    Console.WriteLine("Invalid input. Please enter digits only (0-9).");
+                }
+            } while (!isValid);
+
+            // Convert to integer and add 10
+            number = int.Parse(input) + 10;
+
+            // Display result
+            Console.WriteLine($"Result after adding 10: {number}");
+
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ConvertMe")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConvertMe")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0474c55e-0497-4c59-80d5-43a4c8f71504")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
